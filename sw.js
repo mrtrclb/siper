742b04f39ee43684475344e15ff1651d82de990f
MRTRCLB <72945470+mrtrclb@users.noreply.github.com>
2026-04-20 22:37:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Siper Service Worker v2.3.0
-const CACHE_VERSION = 'siper-v2.3.0';
+// Siper Service Worker v2.4.0
+const CACHE_VERSION = 'siper-v2.4.0';
 const CORE_ASSETS = [
   './',
   './index.html',
