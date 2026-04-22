8e5553f064d1280a55efee5549309fbf464ee9d3
MRTRCLB <72945470+mrtrclb@users.noreply.github.com>
2026-04-22 18:46:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Siper Service Worker
 // NOT: Bu versiyonu index.html içindeki GAME_VERSION ile aynı tut.
-const CACHE_VERSION = 'siper-v2.6.4';
+const CACHE_VERSION = 'siper-v2.7.0';
 const CORE_ASSETS = [
   './',
   './index.html',
